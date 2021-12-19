923abb69e6ba47a6e58c771c3e7b17716e218b63
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2021-12-19 16:36:04 +0900

    improve

--- src/class.lua
+++ src/class.lua
@@ -105,9 +105,7 @@ function module.init(shared)
 					-- this is using hacky of roblox instance
 					-- this is will keep reference from week table until
 					-- inscance got GCed
-					item:GetPropertyChangedSignal("ClassName"):Connect(function ()
-						return regFn;
-					end);
+					item:GetPropertyChangedSignal("ClassName"):Connect(regFn);
 				elseif (valueType == "function" or valueType == "table") and bind(item,index,value,valueType) then -- connect event
 					-- event binding
 				elseif indexType == "string" then
