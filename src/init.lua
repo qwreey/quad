2c5506f17fab0e927181e19d9cf57456ba6af4b0
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2021-12-24 19:56:02 +0900

    error fix

--- src/init.lua
+++ src/init.lua
@@ -46,8 +46,8 @@ function module.init(id)
 	this.event = event.init(this);
 	this.store = store.init(this);
 	--this.style = style.init(this);
-	this.class = class.init(this);
 	this.mount = mount.init(this);
+	this.class = class.init(this);
 
 	--this.makeClass = makeClass;
 	--this.tween = tween;
